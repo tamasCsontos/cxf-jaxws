ed8434acf0db4c7bffa60961047c546c2f9188f9
Csontos Tamás <Tamas.Csontos@dorsum.eu>
2020-11-20 11:28:24 +0100

    com

---
+++ cxf-jaxws-spring-boot/src/main/java/com/microsoft/schemas/_2003/_10/serialization/arrays/ArrayOfdateTime.java
@@ -0,0 +1,70 @@
+
+package com.microsoft.schemas._2003._10.serialization.arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlSchemaType;
+import javax.xml.bind.annotation.XmlType;
+import javax.xml.datatype.XMLGregorianCalendar;
+
+
+/**
+ * &lt;p&gt;Java class for ArrayOfdateTime complex type.
+ * 
+ * &lt;p&gt;The following schema fragment specifies the expected content contained within this class.
+ * 
+ * &lt;pre&gt;
+ * &amp;lt;complexType name="ArrayOfdateTime"&amp;gt;
+ *   &amp;lt;complexContent&amp;gt;
+ *     &amp;lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&amp;gt;
+ *       &amp;lt;sequence&amp;gt;
+ *         &amp;lt;element name="dateTime" type="{http://www.w3.org/2001/XMLSchema}dateTime" maxOccurs="unbounded" minOccurs="0"/&amp;gt;
+ *       &amp;lt;/sequence&amp;gt;
+ *     &amp;lt;/restriction&amp;gt;
+ *   &amp;lt;/complexContent&amp;gt;
+ * &amp;lt;/complexType&amp;gt;
+ * &lt;/pre&gt;
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "ArrayOfdateTime", propOrder = {
+    "dateTime"
+})
+public class ArrayOfdateTime {
+
+    @XmlSchemaType(name = "dateTime")
+    protected List<XMLGregorianCalendar> dateTime;
+
+    /**
+     * Gets the value of the dateTime property.
+     * 
+     * &lt;p&gt;
+     * This accessor method returns a reference to the live list,
+     * not a snapshot. Therefore any modification you make to the
+     * returned list will be present inside the JAXB object.
+     * This is why there is not a &lt;CODE&gt;set&lt;/CODE&gt; method for the dateTime property.
+     * 
+     * &lt;p&gt;
+     * For example, to add a new item, do as follows:
+     * &lt;pre&gt;
+     *    getDateTime().add(newItem);
+     * &lt;/pre&gt;
+     * 
+     * 
+     * &lt;p&gt;
+     * Objects of the following type(s) are allowed in the list
+     * {@link XMLGregorianCalendar }
+     * 
+     * 
+     */
+    public List<XMLGregorianCalendar> getDateTime() {
+        if (dateTime == null) {
+            dateTime = new ArrayList<XMLGregorianCalendar>();
+        }
+        return this.dateTime;
+    }
+
+}
